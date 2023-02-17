65b6c8eb25abc4eb5c5fc4545ee6808b28be007c
Helinä Lähteenmäki <helina.lahteenmaki@helsinki.fi>
2023-02-17 21:57:16 +0200

    exercises 4.11.-4.14.

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -7,9 +7,33 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
-  const savedBlog = await blog.save()
+  const newBlog = new Blog({
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0
+  })
+
+  const savedBlog = await newBlog.save()
   response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const blog = new Blog({
+    _id: request.params.id,
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0
+  })
+  
+  await Blog.findByIdAndUpdate(request.params.id, blog, {new: true})
+  response.status(200).end()
 })
 
 module.exports = blogsRouter
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -29,27 +29,125 @@ test('all blogs have a correct id', async () => {
   })
 })
 
-test('posting a blog adds it correctly', async () => {
-  const newBlog = {
-    title: 'uusi title',
-    author: 'uusi author',
-    url: 'uusi url',
-    likes: 5,
-  }
+describe('addition of a new blog', () => {
+  test('posting a blog adds it correctly', async () => {
+    const newBlog = {
+      title: 'uusi title',
+      author: 'uusi author',
+      url: 'uusi url',
+      likes: 5,
+    }
+      
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+      
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length+1)
+      
+    const contents = blogsAtEnd.map(r => r.title)
+    expect(contents).toContain(
+      'uusi title'
+    )
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+  test('if likes are null then given the value 0', async () => {
+    const newBlog = {
+      title: 'title',
+      author: 'author',
+      url: 'url'
+    }  
+        
+    await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+        
+    const response = await api.get('/api/blogs')
+    const testBlog = response.body[2]
+    expect(testBlog.likes).toBeDefined()
+  })
+      
+  test('if title is null then status code 400', async () => {
+    const newBlog = {
+      author: 'author',
+      url: 'url'
+    }  
+        
+    await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+        
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+        
+  test('is url is null then status code 400', async () => {
+    const newBlog = {
+      title: 'title',
+      author: 'autor'
+    }  
+        
+    await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+        
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+})
 
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length+1)
+describe('deletion of a blog', () => {
+  test('deletion works', async () => {
+    await api.delete('/api/blogs/5a422a851b54a676234d17f7')
+      .expect(204)
+    
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length-1)
+  })
+    
+  test('status code 400 if malformatted id', async () => {
+    await api.delete('/api/blogs/malformattedId')
+      .expect(400)
+    
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+})
 
-  const contents = blogsAtEnd.map(r => r.title)
-  expect(contents).toContain(
-    'uusi title'
-  )
+describe('changing a blog', () => {
+  const changedBlog = {
+    title: 'muokattu title',
+    author: 'muokattu author',
+    url: 'muokattu url',
+    likes: 20
+  }  
+    
+  test('changes a blog correctly', async () => {
+    await api.put('/api/blogs/5a422a851b54a676234d17f7')
+      .send(changedBlog)
+      .expect(200)
+    
+    const response = await api.get('/api/blogs')
+    const blog = response.body[0]
+        
+    expect(blog.title).toBe('muokattu title')
+    expect(blog.likes).toBe(20)
+  })
+    
+  test('status code 400 if malformatted id when trying to change', async () => {
+    await api.put('/api/blogs/malformattedId')
+      .send(changedBlog)
+      .expect(400)
+    
+    const response = await api.get('/api/blogs')
+    const blog = response.body[0]
+    
+    expect(blog.title).toBe('React patterns')
+    expect(blog.likes).toBe(1)
+  })
 })
 
 afterAll(async () => {
