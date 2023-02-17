d79842ee026fa27ea31a25701b266ec219f4fa25
Helinä Lähteenmäki <helina.lahteenmaki@helsinki.fi>
2023-02-17 13:08:35 +0200

    exercises 4.8.-4.10.

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('expected amount of JSON blogs are returned', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
+test('all blogs have a correct id', async () => {
+  const response = await api.get('/api/blogs')
+
+  response.body.forEach(blog => {
+    expect(blog.id).toBeDefined()
+  })
+})
+
+test('posting a blog adds it correctly', async () => {
+  const newBlog = {
+    title: 'uusi title',
+    author: 'uusi author',
+    url: 'uusi url',
+    likes: 5,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length+1)
+
+  const contents = blogsAtEnd.map(r => r.title)
+  expect(contents).toContain(
+    'uusi title'
+  )
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
