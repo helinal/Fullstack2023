3a36838f6042bb97450afa9419882eee6469c33d
Helinä Lähteenmäki <helina.lahteenmaki@helsinki.fi>
2023-03-13 16:27:55 +0200

    exercises 7.14.-7.17.

--- osa7/bloglist/frontend/src/App.js
+++ osa7/bloglist/frontend/src/App.js
@@ -1,18 +1,27 @@
 import { useState, useEffect, useRef } from 'react'
-import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import storageService from './services/storage'
+import userService from './services/users'
 
 import LoginForm from './components/Login'
 import NewBlog from './components/NewBlog'
 import Notification from './components/Notification'
 import Togglable from './components/Togglable'
 
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+    useParams,
+  } from 'react-router-dom'
+
 const App = () => {
     const [blogs, setBlogs] = useState([])
     const [user, setUser] = useState('')
     const [info, setInfo] = useState({ message: null })
+    const [users, setUsers] = useState([])
 
     const blogFormRef = useRef()
 
@@ -23,6 +32,10 @@ const App = () => {
 
     useEffect(() => {
         blogService.getAll().then((blogs) => setBlogs(blogs))
+    }, [])
+
+    useEffect(() => {
+        userService.getAll().then((users) => setUsers(users))
     }, [])
 
     const notifyWith = (message, type = 'info') => {
@@ -94,30 +107,137 @@ const App = () => {
 
     const byLikes = (b1, b2) => b2.likes - b1.likes
 
-    return (
-        <div>
-            <h2>blogs</h2>
-            <Notification info={info} />
+    const Blog = ({ blog }) => {
+        const style = {
+          marginBottom: 2,
+          padding: 5,
+          borderStyle: 'solid',
+        }
+    
+        return (
+          <div style={style} className="blog">
+            <Link to={`/blogs/${blog.id}`}> {blog.title} {blog.author}</Link>
+          </div>
+        )
+    }
+    
+    const BlogView = () => {
+        const id = useParams().id
+        const blog = blogs.find((n) => n.id === id)
+    
+        return (
+          <>
+            <h2>
+              {blog.title} {blog.author}
+            </h2>
             <div>
-                {user.name} logged in
-                <button onClick={logout}>logout</button>
+              {' '}
+              <a href={blog.url}> {blog.url}</a>{' '}
             </div>
-            <Togglable buttonLabel="new note" ref={blogFormRef}>
-                <NewBlog createBlog={createBlog} />
+            <div>
+              likes {blog.likes}
+              <button onClick={() => like(blog)}>like</button>
+            </div>
+            <div>added by {blog.user.name}</div>
+            {user && blog.user.username === user.username && (
+              <button onClick={() => remove(blog)}>delete</button>
+            )}
+          </>
+        )
+    }
+    
+    const HomePage = () => {
+        return (
+          <div>
+            <Togglable buttonLabel="create new" ref={blogFormRef}>
+              <NewBlog createBlog={createBlog} />
             </Togglable>
             <div>
-                {blogs.sort(byLikes).map((blog) => (
-                    <Blog
-                        key={blog.id}
-                        blog={blog}
-                        like={() => like(blog)}
-                        canRemove={user && blog.user.username === user.username}
-                        remove={() => remove(blog)}
-                    />
-                ))}
+              {blogs.sort(byLikes).map((blog) => (
+                <Blog key={blog.id} blog={blog} />
+              ))}
             </div>
-        </div>
-    )
-}
+          </div>
+        )
+    }
+    
+    const User = ({ users }) => {
+        const id = useParams().id
+        const user = users.find((n) => n.id === id)
+    
+        if (!user) {
+          return null
+        }
+        return (
+          <div>
+            <h2>{user.name}</h2>
+            <h4>added blogs</h4>
+            <ul>
+              {user.blogs.map((blog) => {
+                return <li key={blog.id}>{blog.title}</li>
+              })}
+            </ul>
+          </div>
+        )
+    }
+
+    const Users = () => {
+        return (
+          <>
+            <h2>Users</h2>
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>  </th>
+                    <th> blogs created </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((user) => {
+                    return (
+                      <tr key={user.username}>
+                        <td>
+                          <Link to={`/users/${user.id}`}>{user.name}</Link>
+                        </td>
+                        <td>{user.blogs.length}</td>
+                      </tr> )})}
+                </tbody>
+              </table>
+            </div>
+          </>
+        )
+    }
+    
+    const Header = () => {
+        return (
+          <>
+            <div>
+              <Link to="/">blogs</Link> &nbsp;
+              <Link to="/users">users</Link> &nbsp;
+              {user.name} logged in &nbsp;
+              <button onClick={logout}>logout</button>
+            </div>
+
+            <h2>blog app</h2>
+            <Notification info={info} />
+          </>
+        )
+    }
+    
+    return (
+        <>
+          <Router>
+            <Header />
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/users/:id" element={<User users={users} />} />
+              <Route path="/blogs/:id" element={<BlogView />} />
+            </Routes>
+          </Router>
+        </>
+      )
+    }
 
 export default App
